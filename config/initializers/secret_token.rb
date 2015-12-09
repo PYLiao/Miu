a116c777e37a4abb24cf0b30c5687b90ec89f496
PYLiao <lby75429@gmail.com>
2015-12-09 17:49:16 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Miu::Application.config.secret_key_base = '9a859223ee21a6e6ee9dd3e60782c590336ac8dd0dc433113d0c0b5bc288a21f414ee8f1558576e10efd928190db9769c2987ff34e598fcff165c879951b945a'
